0e6ff1f3a2be853e6e32c64d611f86750e1b6f5f
shettychirag7 <shettychirag7@gmail.com>
2022-04-18 12:24:21 +0800

    feat(screen): Home Screen

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -5,23 +5,28 @@ import Text from '../Text';
 import useStylePicker from './listitem.style';
 
 interface ListItemProps extends TouchableOpacityProps {
+  id: number;
   name: string;
   onUpdate?: (value: string) => void;
 }
 
 const ListItem = (props: ListItemProps) => {
-  const {name, onUpdate, ...rest} = props;
+  const {id, name, onUpdate, ...rest} = props;
 
   const styles = useStylePicker();
 
   return (
     <TouchableOpacity {...rest} style={styles.listitem}>
       <TextInput
-        testID="ip_field"
+        testID={`ip_field_${id}`}
         selectTextOnFocus={true}
         style={styles.title}
-        onEndEditing={e => onUpdate?.(e.nativeEvent.text)}>
-        <Text type={'boldBody'}>{name}</Text>
+        onEndEditing={e =>
+          e.nativeEvent.text !== name && onUpdate?.(e.nativeEvent.text)
+        }>
+        <Text testID={`txt_field_${id}`} type={'boldBody'}>
+          {name}
+        </Text>
       </TextInput>
       <Icon name="chevron-right" size={24} />
     </TouchableOpacity>
